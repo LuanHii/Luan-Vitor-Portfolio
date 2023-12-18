1d7598f4837543b08cc2efd69d50f84ae7a2c916
LuanHii <luanhiivitor@gmail.com>
2023-12-18 00:55:51 -0300

    Updated

--- script/script.js
+++ script/script.js
@@ -1,48 +1,52 @@
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
-    var links = document.querySelectorAll('.link');
-    var descricaoElement = document.getElementById('desc');
-    var imgElements = document.querySelectorAll('.desc-img');
+  var links = document.querySelectorAll('.link');
+  var descricaoElement = document.getElementById('desc');
+  var imgElements = document.querySelectorAll('.desc-img');
+  var gitButton = document.getElementById('git-button');
 
-    function updateDescricao(novaDescricao, novasImagens) {
-      descricaoElement.style.opacity = 0;
-      descricaoElement.addEventListener('transitionend', function() {
-        descricaoElement.textContent = novaDescricao;
-        descricaoElement.style.opacity = 1;
-      }, { once: true });
+  function updateDescricao(novaDescricao, novasImagens, novoLink) {
+    descricaoElement.style.opacity = 0;
+    descricaoElement.addEventListener('transitionend', function() {
+      descricaoElement.textContent = novaDescricao;
+      descricaoElement.style.opacity = 1;
+    }, { once: true });
 
-      links.forEach(link => link.classList.remove('selected'));
-      this.classList.add('selected');
+    links.forEach(link => link.classList.remove('selected'));
+    this.classList.add('selected');
 
-      updateImagens(novasImagens);
-    }
+    updateImagens(novasImagens);
 
-    function updateImagens(novasImagens) {
-      imgElements.forEach((img, index) => {
-        img.style.opacity = 0;
-        img.addEventListener('transitionend', function() {
-          img.src = novasImagens[index];
-          img.style.opacity = 1;
-        }, { once: true });
-      });
-    }
+    gitButton.href = novoLink;
+  }
 
-    links.forEach(link => {
-      link.addEventListener('click', function(event) {
-        event.preventDefault();
-        var novaDescricao = this.getAttribute('data-descricao');
-        var novasImagens = JSON.parse(this.getAttribute('data-imagens'));
-        updateDescricao.call(this, novaDescricao, novasImagens);
-      });
-    });
-
-    var descricaoInicial = document.querySelector('.link.selected').getAttribute('data-descricao');
-    var imagensIniciais = JSON.parse(document.querySelector('.link.selected').getAttribute('data-imagens'));
-    descricaoElement.textContent = descricaoInicial;
-
+  function updateImagens(novasImagens) {
     imgElements.forEach((img, index) => {
-      img.src = imagensIniciais[index];
+      img.style.opacity = 0;
+      img.addEventListener('transitionend', function() {
+        img.src = novasImagens[index];
+        img.style.opacity = 1;
+      }, { once: true });
+    });
+  }
+
+  links.forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      var novaDescricao = this.getAttribute('data-descricao');
+      var novasImagens = JSON.parse(this.getAttribute('data-imagens'));
+      var novoLink = this.getAttribute('data-github');
+      updateDescricao.call(this, novaDescricao, novasImagens, novoLink);
     });
   });
+
+  var descricaoInicial = document.querySelector('.link.selected').getAttribute('data-descricao');
+  var imagensIniciais = JSON.parse(document.querySelector('.link.selected').getAttribute('data-imagens'));
+  var linkInicial = document.querySelector('.link.selected').getAttribute('data-github');
+  descricaoElement.textContent = descricaoInicial;
+
+  imgElements.forEach((img, index) => {
+    img.src = imagensIniciais[index];
+  });
+
+  gitButton.href = linkInicial;
+});
